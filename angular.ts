4bca6503186a3b50fb4d7a0dccd7fd1ca946f918
Dhanyashree <75880251+dhanyashree-karkera@users.noreply.github.com>
2024-10-22 17:12:20 +0530

    angular.ts

--- angular.ts
+++ angular.ts
@@ -19,3 +19,63 @@
   showMessage(message:string){
     alert(message);
   }
+
+
+
+// DataBinding
+ 
+<!-- Interpolation -->
+<div class="row">
+    <div class="col-3">
+        <p>{{firstName}}</p>
+    </div>
+    <div class="col-3">
+        <p>{{angularVersion}}</p><p>{{version}}</p>
+    </div>
+    <div class="col-3">
+        <p>{{isActive}}</p> 
+    </div>
+    <div class="col-3">
+        <p>{{currentDate}}</p>
+    </div>
+    <div class="col-3">
+        <p>{{selectedState}}</p>
+    </div>
+</div>
+
+<!-- property binding -->
+<div class="row">
+    <div class="col-3">
+        <input type="text" [value]="firstName" class="form-control"/>
+    </div>
+    <div class="col-3">
+        <input [type]="inputType"/>
+    </div>
+
+    <!-- two way data binding -->
+    <div class="col-3">
+        <input type="text" [(ngModel)]="firstName" class="form-control"/>
+    </div>
+    <div class="col-3">
+        <select [(ngModel)]="selectedState">
+            <option value="A">Hyy</option>
+            <option value="B">Byee</option>
+            <option value="C">Tata</option>
+        </select>
+    </div>
+</div>
+
+<!-- event binding -->
+<div class="row" style="margin-top: 2rem;">
+    <div class="col-3">
+        <button class="btn btn-success" (click)="showFunctionName()">Submit</button>
+    </div>
+    <div class="col-3">
+        <button class="btn btn-danger" (click)="showMessage('welcome message')">Cancel</button>
+    </div>
+</div>
+
+
+
+
+
